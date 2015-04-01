6bd87b8438a59d143def9f3b495fd05869432a18
Hsu-Chieh Hu <hsuchieh.hu@gmail.com>
2015-03-31 21:23:23 -0400

    Add presence drawing

--- client.py
+++ client.py
@@ -3,47 +3,56 @@ import parse_XCam
 import sys
 import cv2
 import logging
+import numpy as np
 
+
+#Client parameters
+SERVER_IP = '192.168.123.252'
+SERVER_PORT = 20800
+POLYGONS_FILE = 'OSD.xml'
+STARTLIVE_FILE = 'StartLive'
+buf_size = 512
+hsize = 4
+#Output files
+VIDEO_FILE = 'output_video.avi'
+STATEVECTOR_FILE = 'state_vector.log'
+IMAGE_FILE = 'image.log'
 
 logging.basicConfig(filename='debug.log',level=logging.INFO)
-
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('192.168.123.252', 20800))
+client_socket.connect((SERVER_IP, SERVER_PORT))
 print "start client..."
 
-fi = open('StartLive', 'r')
-state_vector = open('state_vector.log', 'w')
-image_file = open('image.log', 'w')
+fi = open(STARTLIVE_FILE, 'r')
+state_vector = open(STATEVECTOR_FILE, 'w')
+image_file = open(IMAGE_FILE, 'w')
 
 input_data = fi.readline()
 client_socket.send(input_data)
 print 'send StartLive...'
 
-buf_size = 512
-hsize = 4
 residule_header = ''
 fourcc = cv2.cv.CV_FOURCC('D','I','V','X')
-output_video = cv2.VideoWriter('output_video.avi',fourcc, 10.0, (320, 240))
+output_video = cv2.VideoWriter(VIDEO_FILE,fourcc, 10.0, (320, 240))
+points = parse_XCam.parse_Polygons(POLYGONS_FILE)
+num_polygons =  points.size/(4*2)
+statebits = '0'*num_polygons
+statebits_timestamp = '0'
 
 while 1:
     client_socket.send(input_data)
     data = client_socket.recv(buf_size)
-    
     if residule_header != '':
         logging.debug('residule: ' + residule_header)
         data = residule_header + data
         residule_header = ''
-    
     data_size, index = len(data), 0
-    
-    while data_size != 0:
-        
+
+    while data_size != 0:        
         XML_size =  parse_XCam.parse_XCamheader(data[index:index+hsize])
         if XML_size == -1:
             break
-        
         data_size, index = data_size-hsize, index+hsize
-        
         if data_size >= XML_size:
                 XML_message = data[index:index+XML_size]
                 data_size, index = data_size - XML_size , index + XML_size
@@ -56,32 +65,31 @@ while 1:
                 XML_message = data[index:]
                 rest_XML_size = XML_size - len(XML_message)
                 while (rest_XML_size != 0):
-                        data = client_socket.recv(buf_size)
-                        
+                        data = client_socket.recv(buf_size)            
                         data_size, index = len(data), 0
-
                         if data_size >= rest_XML_size:
                                 XML_message += data[:rest_XML_size]
                                 data_size, index = data_size - rest_XML_size , index + rest_XML_size
                                 rest_XML_size = 0
-                                
                                 if data_size < hsize and data_size > 0:
                                     logging.debug('residule 2 data_size: ' + str(data_size))
                                     residule_header = data[index:]
                                     data_size = 0
-
                         else:
                                 XML_message += data[:]
                                 rest_XML_size, data_size = rest_XML_size - data_size, 0
 
         logging.debug('parsed_message: '+XML_message)
-        
         parsed_message = parse_XCam.parse_LiveImage(XML_message)
         if(parsed_message != -1):
             image_file.write(XML_message+'\n\n')
-            video_timestamp = parse_XCam.parse_TimeStamp(XML_message)
             img = parse_XCam.output_image(parsed_message)
-            cv2.putText(img,str(video_timestamp), (30,45), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255))
+            cv2.putText(img,str(statebits_timestamp), (30,45), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255))
+            for i in range(num_polygons):
+                if statebits[i] == '0':
+                    cv2.polylines(img, np.int32([points[i]]), 1, (0,0,255), 1)
+                else:
+                    cv2.polylines(img, np.int32([points[i]]), 1, (255,0,0), 1)
             output_video.write(img)
             cv2.imshow('XCam-p', img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -89,11 +97,10 @@ while 1:
        
         parsed_message = parse_XCam.parse_StateVector(XML_message)
         if(parsed_message != -1):
+            statebits_timestamp = parsed_message[:parsed_message.find(' ')]
+            statebits = parsed_message[parsed_message.find(' ')+1:-1]
             state_vector.write(parsed_message)
 
-
-
-print 'leave client...'
 video_write.release()
 cv2.destroyAllWindows()                      
 
--- parse_XCam.py
+++ parse_XCam.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import base64
+import xml.etree.ElementTree as ET
 
 fgbg = cv2.BackgroundSubtractorMOG()
 
@@ -16,7 +17,7 @@ def parse_XCamheader(s):
 
 def parse_StateVector(s):
 	state_vector = '<StateVector>'
-	num_bits_StateVector = 5
+	num_bits_StateVector = 7
 
 	sv_index = s.find(state_vector)
 	ts_return = parse_TimeStamp(s)
@@ -46,12 +47,28 @@ def parse_LiveImage(s):
 	else:
 		return -1
 
-
 def output_image(s):
 	output_data = base64.b64decode(s)
 	array = np.frombuffer(output_data, dtype='uint8')
 	img = cv2.imdecode(array, 1)
 	#img = fgbg.apply(img)
 	return img
+
+def parse_Polygons(filename):
+	tree = ET.parse(filename)
+	root = tree.getroot()
+	points = np.zeros(shape=(1,2))
+	for neighbor in root.iter('P'):
+		points = np.append(points,[[int(neighbor.attrib['y'])/2, int(neighbor.attrib['x'])/2 ]],0)
+	points = np.delete(points ,0 ,0)
+	ret = np.zeros(shape=(4,2))
+	ret = np.array([ret])
+	for  i  in range(points.size/(4*2)):
+		ret = np.append(ret,[points[4*i:4*(i+1)]],0)
+	ret = np.delete(ret ,0 ,0)
+	return ret
+	
+
+
     
             
